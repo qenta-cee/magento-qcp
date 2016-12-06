54025651c9fec31c84d76f4b9f1a2346a459cd54
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2016-12-06 12:10:49 +0100

    #6 Add consumerMerchandCrmId and improve security

--- app/code/community/Wirecard/CheckoutPage/Model/Abstract.php
+++ app/code/community/Wirecard/CheckoutPage/Model/Abstract.php
@@ -53,7 +53,7 @@ abstract class Wirecard_CheckoutPage_Model_Abstract extends Mage_Payment_Model_M
     protected $_defaultLocale = 'en';
 
     protected $_order;
-    protected $_pluginVersion = '4.0.5';
+    protected $_pluginVersion = '4.1.0';
     protected $_pluginName = 'Wirecard/CheckoutPage';
 
     protected $_formBlockType = 'wirecard_checkoutpage/form';
@@ -165,6 +165,7 @@ abstract class Wirecard_CheckoutPage_Model_Abstract extends Mage_Payment_Model_M
             ->setCancelUrl($returnUrl)
             ->setFailureUrl($returnUrl)
             ->setServiceUrl($helper->getConfigData('options/serviceurl'))
+            ->createConsumerMerchantCrmId($this->getOrder()->getCustomerEmail())
             ->setConsumerData($this->_getConsumerData());
 
         $init->setMaxRetries($helper->getConfigData('options/maxretries'));
